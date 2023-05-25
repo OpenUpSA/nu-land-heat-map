ee77edc9e2de7858ac007958371b52cd5ee7a484
paulmwatson <github.com@paulmwatson.com>
2023-05-25 23:01:02 +0200

    Clean up

--- src/map.js
+++ src/map.js
@@ -1,56 +1,21 @@
 import geojson from "./data/parcels.json";
-import * as mapStyles from "./config/map-style.json";
 import * as heatmapGradient from "./config/heatmap-gradient.json";
-import { getScaledValue } from "./utils.js";
+import * as mapStyles from "./config/map-style.json";
+import { buildHeatMapData, filterByProperty } from "./utils.js";
 import { Loader } from "@googlemaps/js-api-loader";
 
 const urlSearch = new URLSearchParams(window.location.search);
-const filtered = (urlSearch.get("filtered") || "").split(",");
+const selected = (urlSearch.get("filtered") || "").split(",");
 const loader = new Loader({
   apiKey: "AIzaSyDNhC5KPQu7govGn9bXQOF1PE3mjKTrctg",
   version: "weekly",
   libraries: ["visualization"],
 });
 
-let map;
-let min = 0;
-let max = 0;
-
-geojson["features"].forEach((feature) => {
-  const area = feature["properties"]["Size m2"];
-  if (area > max) {
-    max = area;
-  }
-  if (area < min) {
-    min = area;
-  }
-});
-
-loader.load().then(async () => {
+const initMap = async () => {
   const { Map } = await google.maps.importLibrary("maps");
 
-  if (filtered[0] !== "") {
-    geojson["features"] = geojson["features"].filter((feature) => {
-      return filtered.includes(feature["properties"]["Owner type"]);
-    });
-  }
-
-  let heatmapData = [];
-  geojson["features"].forEach((feature) => {
-    let latlng = new google.maps.LatLng(
-      feature["geometry"]["coordinates"][1],
-      feature["geometry"]["coordinates"][0]
-    );
-    heatmapData.push({
-      location: latlng,
-      weight: urlSearch.get("byarea")
-        ? getScaledValue(feature["properties"]["Size m2"], min, max, 0, 100)
-        : 1,
-      maxIntensity: 1,
-    });
-  });
-
-  map = new Map(document.getElementById("map"), {
+  let googleMap = new Map(document.getElementById("map"), {
     center: new google.maps.LatLng(-34, 18.5241),
     zoom: 12,
     maxZoom: 14,
@@ -63,11 +28,17 @@ loader.load().then(async () => {
     mapTypeId: google.maps.MapTypeId.TERRAIN,
   });
 
-  var heatmap = new google.maps.visualization.HeatmapLayer({
+  geojson["features"] = filterByProperty(geojson["features"], selected);
+
+  let heatmapData = buildHeatMapData(geojson, urlSearch.get("byarea"));
+
+  let heatmap = new google.maps.visualization.HeatmapLayer({
     data: heatmapData,
     radius: parseInt(urlSearch.get("radius")) || 25,
     gradient: heatmapGradient,
     maxIntensity: parseInt(urlSearch.get("maxintensity")) || 75,
   });
-  heatmap.setMap(map);
-});
+  heatmap.setMap(googleMap);
+};
+
+loader.load().then(initMap);
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,64 @@
 export const getScaledValue = (
-    value,
-    sourceRangeMin,
-    sourceRangeMax,
-    targetRangeMin,
-    targetRangeMax
-  ) => {
-    var targetRange = targetRangeMax - targetRangeMin;
-    var sourceRange = sourceRangeMax - sourceRangeMin;
+  value,
+  sourceRangeMin,
+  sourceRangeMax,
+  targetRangeMin,
+  targetRangeMax
+) => {
+  var targetRange = targetRangeMax - targetRangeMin;
+  var sourceRange = sourceRangeMax - sourceRangeMin;
+  return (
+    ((value - sourceRangeMin) * targetRange) / sourceRange + targetRangeMin
+  );
+};
+
+export const getMinMax = (features) => {
+  let min = 0;
+  let max = 0;
+  features.forEach((feature) => {
+    const area = feature["properties"]["Size m2"];
+    if (area > max) {
+      max = area;
+    }
+    if (area < min) {
+      min = area;
+    }
+  });
+  return [min, max];
+};
+
+export const filterByProperty = (features, selected) => {
+  return features.filter((feature) => {
     return (
-      ((value - sourceRangeMin) * targetRange) / sourceRange + targetRangeMin
+      selected[0] === "" ||
+      selected.includes(feature["properties"]["Owner type"])
     );
-  };
-  
+  });
+};
+
+export const buildHeatMapData = (geojson, byArea) => {
+  let minMax = getMinMax(geojson["features"]);
+
+  let heatmapData = [];
+
+  geojson["features"].forEach((feature) => {
+    let latlng = new google.maps.LatLng(
+      feature["geometry"]["coordinates"][1],
+      feature["geometry"]["coordinates"][0]
+    );
+    heatmapData.push({
+      location: latlng,
+      weight: byArea
+        ? getScaledValue(
+            feature["properties"]["Size m2"],
+            minMax[0],
+            minMax[1],
+            0,
+            100
+          )
+        : 1,
+      maxIntensity: 1,
+    });
+  });
+  return heatmapData;
+};
